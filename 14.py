c362820c22c767e0caf36cbb97d676f8e3c07ede
AllVolLOAD <juniormiddlehead@yandex.ru>
2022-08-09 14:46:51 +0300

    second try

---
+++ 14.py
@@ -0,0 +1,39 @@
+def recurs(x, count):
+    if count > 990:
+        return count
+    
+    if x == 1:
+        count +=1
+        return count
+    
+    elif x%2==0:
+        x = x/2
+        count +=1
+        return recurs(x, count)
+
+    else:
+        x = 3 * x + 1
+        count +=1
+        return recurs(x, count)
+
+count = 0
+
+dic = {}
+
+
+for _ in range(999999, 99999, -1):
+    func = recurs(_, count)
+    dic[_]=func
+    if len(dic)>500:
+        max_val = max(dic.values())
+        dic.clear() 
+        dic[_]=func
+        print(dic)
+    count = 0
+
+
+
+        
+
+
+    
